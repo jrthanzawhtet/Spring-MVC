8a54100db1795140616d1fdd2f0a18f352fe0ccf
jrthanzawhtet <thanzawhtet18@gmail.com>
2024-11-28 23:59:18 +0630

    config

---
+++ spring-web-mvc/ep04-spring-forms/src/main/java/com/jdc/spring/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.jdc.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(
+		)
+public class JpaConfig {
+
+}
